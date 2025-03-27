54cfdf8aabf53801014c7186cdd9abf72b20b133
chiarasanasi <chiarasanasi.work@gmail.com>
2025-03-27 17:35:38 +0100

    WIP

---
+++ script/nav-bar.js
@@ -0,0 +1,22 @@
+// const volume = document.getElementById("volume")
+// const volumeSlider = document.getElementById("volume-slider")
+
+// // console.log(volume, volumeSlider, window.innerWidth)
+
+// const changeVolumeSliderPos = function () {
+//   if (window.innerWidth < 768) {
+//     volumeSlider.classList.add("rotate-90", "d-none")
+//     volume.addEventListener("mouseenter", () => {
+//       volumeSlider.classList.remove("d-none")
+//     })
+//     volume.addEventListener("mouseleave", () => {
+//       volumeSlider.classList.add("d-none")
+//     })
+//   } else {
+//     volumeSlider.classList.remove("rotate-90", "d-none")
+//   }
+// }
+// changeVolumeSliderPos()
+// window.addEventListener("resize", changeVolumeSliderPos)
+
+// volume.addEventListener("")
