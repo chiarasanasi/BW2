7826df0fb6afa149edaa519603939c9266bf7e1b
Pierattilio-correale <pierattiliocorreale@yahoo.it>
2025-03-28 14:59:33 +0100

    push

--- script/artist.js
+++ script/artist.js
@@ -25,7 +25,7 @@ const artistAPI = function () {
                 <div class="col-6 col-md-6 mb-2 mb-md-0">
                   <div class="d-flex w-100">
 
-                    <a href="/album2.html" class="btn btn-light me-2 bg-transparent border-0 text-light-emphasis">
+                    <button onclick="history.back()" class="btn btn-light me-2 bg-transparent border-0 text-light-emphasis">
                       <svg
                         xmlns="http://www.w3.org/2000/svg"
                         width="25"
@@ -37,9 +37,9 @@ const artistAPI = function () {
                           d="M8 0a8 8 0 1 0 0 16A8 8 0 0 0 8 0m3.5 7.5a.5.5 0 0 1 0 1H5.707l2.147 2.146a.5.5 0 0 1-.708.708l-3-3a.5.5 0 0 1 0-.708l3-3a.5.5 0 1 1 .708.708L5.707 7.5z"
                         />
                       </svg>
-                    </a>
+                    </button>
 
-                    <a href="/artist.html?id=${artistId}" class="btn me-2 bg-transparent border-0 text-light-emphasis">
+                    <button onclick="history.forward()" class="btn me-2 bg-transparent border-0 text-light-emphasis">
                       <svg
                         xmlns="http://www.w3.org/2000/svg"
                         width="25"
@@ -51,7 +51,7 @@ const artistAPI = function () {
                           d="M8 0a8 8 0 1 1 0 16A8 8 0 0 1 8 0M4.5 7.5a.5.5 0 0 0 0 1h5.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3a.5.5 0 0 0 0-.708l-3-3a.5.5 0 1 0-.708.708L10.293 7.5z"
                         />
                       </svg>
-                    </a>
+                    </button>
 
                   </div>
                 </div>
